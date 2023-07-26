fb4adaa223feed9f4d67a2eb33decd12640b0113
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-26 16:08:08 -0400

    Add updateScore, newGame, resetScore

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 let rockButton = document.querySelector('.rock');
 let paperButton = document.querySelector('.paper');
 let scissorsButton = document.querySelector('.scissors');
 
+let textBox = document.querySelector('.text');
 let winMessage = document.querySelector('.win-message');
 let playerScoreBox = document.querySelector('.player-score');
 let computerScoreBox = document.querySelector('.computer-score');
@@ -56,14 +57,53 @@ function playRound(playerSelection, computerSelection) {
     winMessage.textContent = result;
 }
 
+function newGame() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+
+    if (computerScore === 5) {
+        winMessage.textContent = "Computer wins!"
+    }
+    else {
+        winMessage.textContent = "Player wins!"
+    }
+    playAgainButton = document.createElement('button');
+    playAgainButton.textContent = "Play again?";
+    textBox.appendChild(playAgainButton);
+    playAgainButton.addEventListener('click', resetScore);
+}
+
+function resetScore() {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+
+    computerScore = 0;
+    playerScore = 0;
+    updateScore();
+    
+    winMessage.textContent = "";
+    textBox.removeChild(playAgainButton);
+}
+
+function updateScore() {
+    if (playerScore >= 5 || computerScore >= 5) newGame();
+    playerScoreBox.textContent = playerScore;
+    computerScoreBox.textContent = computerScore;
+}
+
 rockButton.addEventListener('click', () => {
     playRound('rock', getComputerChoice());
 })
+rockButton.addEventListener('click', updateScore);
 
 paperButton.addEventListener('click', () => {
     playRound('paper', getComputerChoice());
 })
+paperButton.addEventListener('click', updateScore);
 
 scissorsButton.addEventListener('click', () => {
     playRound('scissors', getComputerChoice());
 })
+scissorsButton.addEventListener('click', updateScore);
